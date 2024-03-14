2dcdf134f2e46a838c675a61899f084b16fd5b99
priscila204 <priscila.silva12@estudante.ifms.edu.br>
2024-03-13 20:24:17 -0400

    atividade 12/03/24
    ...

---
+++ src/Componentes/Rodapé.jsx
@@ -0,0 +1,13 @@
+ let função padrão de exportação Rodapé () 
+    retornar 
+    < Rodapé > {
+
+   <><><><h1> feito por priscila </h1>
+            <h1> atividade </h1></>
+            <h1> 1.quantos anos tem o Brasil ? </h1></>
+            </><h2> @ spriscilafer</h2></>
+
+            </ Rodapé >
+    ;
+}
+
